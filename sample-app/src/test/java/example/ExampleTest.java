4e264e48c7bd718ceb3794dae85e7e83955c59f7
Chien <chien@yahoo.com>
2018-11-15 22:14:05 +0700

    add

--- sample-app/src/test/java/example/ExampleTest.java
+++ sample-app/src/test/java/example/ExampleTest.java
@@ -1,5 +1,6 @@
 package example;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -26,6 +27,23 @@ public class ExampleTest {
 		assertTrue(true);
 		log.info("Stop testJoinSecond ... DONE.");
     }	
+	
+	@Test
+    public void testJoinThird() {
+		log.info("Start testJoinThird ... ");
+		assertEquals("Joined string didn't match", "5 6", Example.join("5", "6"));		
+		assertTrue(true);
+		log.info("Stop testJoinThird ... DONE.");
+    }	
+	
+	@Test
+    public void testJoinFourth() {
+		log.info("Start testJoinFourth ... ");
+		assertEquals("Joined string didn't match", "5 6", Example.join("5", "6"));		
+		assertTrue(true);
+		log.info("Stop testJoinFourth ... DONE.");
+    }	
+	
 	@Test
     public void testFailChien() {
 		log.info("Start testFailChien ... ");
@@ -34,4 +52,38 @@ public class ExampleTest {
 		log.info("Stop testFailChien ... DONE.");
     }
 	
+	@Test
+    public void testFailThuHai() {
+		log.info("Start testFailThuHai ... ");
+		assertEquals("Not equals for Values 12222...", "Duong xua chung bong", "Duong xua 123 chung bong");		
+		assertTrue(false);
+		log.info("Stop testFailThuHai ... DONE.");
+    }
+	
+	@Test
+	@Ignore("Thich thi ignore thoi")
+    public void testIgnoreThuNhat() {
+		log.info("Start testIgnoreThuNhat ... ");
+		assertEquals("Not equals for Values 12222...", "Duong xua chung bong", "Duong xua 123 chung bong");		
+		assertTrue(false);
+		log.info("Stop testIgnoreThuNhat ... DONE.");
+    }
+	
+	@Test
+	@Ignore("Thich thi ignore thoi 2222")
+    public void testIgnoreThuHai() {
+		log.info("Start testIgnoreThuHai ... ");
+		assertEquals("Not equals for Values 12222...", "Duong xua chung bong", "Duong xua 123 chung bong");		
+		assertTrue(false);
+		log.info("Stop testIgnoreThuHai ... DONE.");
+    }
+	
+	@Test
+	@Ignore("Thich thi ignore thoi 333")
+    public void testIgnoreThuBa() {
+		log.info("Start testIgnoreThuBa ... ");
+		assertEquals("Not equals for Values 12222...", "Duong xua chung bong", "Duong xua 123 chung bong");		
+		assertTrue(false);
+		log.info("Stop testIgnoreThuBa ... DONE.");
+    }		
 }
